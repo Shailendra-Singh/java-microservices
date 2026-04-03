d9086fb75a7b7962beacd4027feb77ec86b03c0c
Shailendra <dm@shail.me>
2026-04-03 01:40:38 -0500

    added initial spring project

---
+++ MyBoutique/src/test/java/me/shail/MyBoutique/MyBoutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shail.MyBoutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
