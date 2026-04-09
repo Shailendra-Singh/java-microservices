85fb2216f3c6b89abcb4cbff90bd73cbfb9078d5
Shailendra <shail-singh@outlook.com>
2026-04-09 04:25:13 -0500

    feat: add microservice product-service

---
+++ product-service/src/main/java/me/shail/product_service/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package me.shail.product_service.repository;
+
+import me.shail.product_service.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ product-service/src/main/java/me/shail/product_service/service/CategoryService.java
@@ -0,0 +1,65 @@
+package me.shail.product_service.service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.shail.myboutique_commons.dto.CategoryDto;
+import me.shail.product_service.model.Category;
+import me.shail.product_service.repository.CategoryRepository;
+
+@AllArgsConstructor
+@Slf4j
+@Service
+@Transactional
+@CircuitBreaker(name = "category-service")
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryDto> findAll() {
+        log.debug("Request to get all categories");
+        return this.categoryRepository.findAll()
+                .stream()
+                .map(CategoryService::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDto findById(Long id) {
+        log.debug("Request to get Category : {}", id);
+        return this.categoryRepository.findById(id)
+                .map(CategoryService::mapToDto)
+                .orElseThrow(IllegalStateException::new);
+    }
+
+    public CategoryDto create(CategoryDto categoryDto) {
+        log.debug("Request to create Category: {}", categoryDto);
+        return mapToDto(this.categoryRepository.save(
+                new Category(categoryDto.name(), categoryDto.description(), Collections.emptySet())));
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Category: {}", id);
+        this.categoryRepository.deleteById(id);
+    }
+
+    public static CategoryDto mapToDto(Category category) {
+        if (category != null) {
+            return new CategoryDto(
+                    category.getId(),
+                    category.getName(),
+                    category.getDescription(),
+                    category.getProducts()
+                            .stream().map(ProductService::mapToDto)
+                            .collect(Collectors.toSet()));
+        }
+
+        return null;
+    }
+}
